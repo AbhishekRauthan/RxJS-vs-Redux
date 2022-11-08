7d019adf0373dd26161b84c7fbfd2ab514d0bd94
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-11-08 15:05:19 +0530

    wrapped app with browser router and mantine provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
+import { MantineProvider } from "@mantine/core";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <MantineProvider withNormalizeCSS withGlobalStyles>
+        <App />
+      </MantineProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
