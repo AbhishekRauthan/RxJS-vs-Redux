fb714cd035259f1aecf4904f2a1516ace21f3cb8
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-11-09 13:23:46 +0530

    created and imported Home, Redux, RxJS pages and created routes using react router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Redux from "./pages/redux";
+import RxJS from "./pages/rxjs";
 import Navbar from "./ui/Navbar";
 
 function App() {
@@ -6,6 +10,11 @@ function App() {
     <>
       <CssBaseline />
       <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/redux" element={<Redux />} />
+        <Route path="/rxjs" element={<RxJS />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/rxjs/index.tsx
@@ -0,0 +1,7 @@
+const RxJS = () => {
+  return (
+    <div>RxJS</div>
+  )
+}
+
+export default RxJS
